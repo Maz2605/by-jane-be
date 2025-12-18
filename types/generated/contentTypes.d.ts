94fdcb0cb19b43cb3fd5a7aa713ffc1bbb64c9d8
Maz2605 <vannam2654@gmail.com>
2025-12-19 04:46:36 +0700

    add fiel gallery and excerpt in arcticle table

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -450,6 +450,11 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     description: Schema.Attribute.Text;
+    excerpt: Schema.Attribute.Text;
+    gallery: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
     isFeatured: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
